354fb666c9b488b18b91b6a3b99a42e037475f47
Chloe3c <kristianishenko21@gmail.com>
2024-10-09 16:28:39 +0200

    Добавьте файлы проекта.

---
+++ LABA1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.Write("Введите номер квартиры: ");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Этаж={(x - 1)/4+1}");
+    Console.WriteLine($"Квартира {(x-1)%4+1} на этаже");
+
+
+}
+catch
+{
+    Console.WriteLine("Введите правильно!");
+}
